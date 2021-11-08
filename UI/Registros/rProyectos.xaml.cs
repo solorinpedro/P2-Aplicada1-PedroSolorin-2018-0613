97590074d1ecd4734f47dd1623e7bba125c2767f
Pedro Luis Solorin Zorilla <pedro_solorin@ucne.edu.do>
2021-11-08 19:53:35 -0400

    programando los botones del registro

--- UI/Registros/rProyectos.xaml.cs
+++ UI/Registros/rProyectos.xaml.cs
@@ -21,15 +21,119 @@ namespace P2_Aplicada1_PedroSolorin_2018_0613.UI.Registros
     /// </summary>
     public partial class rProyectos : Window
     {
-        private TiposTareas Tareas = new TiposTareas();
+        private Proyectos proyectos = new Proyectos();
         public rProyectos()
         {
             InitializeComponent();
-            this.DataContext = Tareas;
+            this.DataContext = proyectos;
 
-            TareaIdComboBox.ItemsSource = TipoTareasBLL.GetTareas();
-            TareaIdComboBox.SelectedValuePath = "TareasId";
-            TareaIdComboBox.DisplayMemberPath = "Descripcion";
+            DescripcionComboBox.ItemsSource = TipoTareasBLL.GetTareas();
+            DescripcionComboBox.SelectedValuePath = "TareasId";
+            DescripcionComboBox.DisplayMemberPath = "Descripcion";
+        }
+        private void Cargar()
+        {
+            this.DataContext = null;
+            this.DataContext = proyectos;
+        }
+        private void Limpiar()
+        {
+            this.proyectos = new Proyectos();
+            this.DataContext = proyectos;
+        }
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            Proyectos encontrado = ProyectosBLL.Buscar(Utilidades.ToInt(ProyectoIdTextBox.Text));
+            if(encontrado != null)
+            {
+                proyectos = encontrado;
+                Cargar();
+            }
+            else
+            {
+                Limpiar();
+                MessageBox.Show("No existe en la base de datos", "Fallo", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void AgregarButton_Click(object sender, RoutedEventArgs e)
+        {
+            proyectos.Detalles.Add(new ProyectoDetalle
+            {
+                ProyectoId = proyectos.ProyectoId,
+                TiposTareas = (TiposTareas)DescripcionComboBox.SelectedItem
+
+            });
+            Cargar();
+        }
+
+        private void RemoverButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(DetalleDataGrid.Items.Count >= 1 && DetalleDataGrid.SelectedIndex <= DetalleDataGrid.Items.Count - 1)
+            {
+                proyectos.Detalles.RemoveAt(DetalleDataGrid.SelectedIndex);
+                Cargar();
+            }
+        }
+
+        private void NuevoButton_Click(object sender, RoutedEventArgs e)
+        {
+            Limpiar();
+        }
+
+        private void GuardarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var paso = ProyectosBLL.Guardar(proyectos);
+            if (paso)
+            {
+                Limpiar();
+                MessageBox.Show("Guardado con exito", "Exito", 
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("No se pudo guardar ", "fallo", 
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ProyectosBLL.Eliminar(Utilidades.ToInt(ProyectoIdTextBox.Text)))
+            {
+                Limpiar();
+                MessageBox.Show("Registro Eliminado", "Exito", 
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("No fue posible Eliminar", "Fallo", 
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void DescripcionComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(DescripcionComboBox.SelectedIndex == 0)
+            {
+                RequerimientoTextBox.Text = ProyectosBLL.BuscarRequerimiento(1);
+                TiempoTextBox.Text = Convert.ToString(ProyectosBLL.BuscarTiempo(1));
+            }
+            else if (DescripcionComboBox.SelectedIndex == 1)
+            {
+                RequerimientoTextBox.Text = ProyectosBLL.BuscarRequerimiento(2);
+                TiempoTextBox.Text = Convert.ToString(ProyectosBLL.BuscarTiempo(2));
+            }
+            else if (DescripcionComboBox.SelectedIndex == 2)
+            {
+                RequerimientoTextBox.Text = ProyectosBLL.BuscarRequerimiento(3);
+                TiempoTextBox.Text = Convert.ToString(ProyectosBLL.BuscarTiempo(3));
+            }
+            else if (DescripcionComboBox.SelectedIndex == 3)
+            {
+                RequerimientoTextBox.Text = ProyectosBLL.BuscarRequerimiento(4);
+                TiempoTextBox.Text = Convert.ToString(ProyectosBLL.BuscarTiempo(4));
+            }
         }
     }
 }
