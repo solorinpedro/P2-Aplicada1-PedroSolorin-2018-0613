74e371b12eecd62a4901ea2e42eca9d0da98886a
Pedro Luis Solorin Zorilla <pedro_solorin@ucne.edu.do>
2021-11-08 20:53:58 -0400

    Haciendo consulta de proyecto

--- UI/Consultas/cProyecto.xaml.cs
+++ UI/Consultas/cProyecto.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using P2_Aplicada1_PedroSolorin_2018_0613.BLL;
+using P2_Aplicada1_PedroSolorin_2018_0613.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +24,39 @@ namespace P2_Aplicada1_PedroSolorin_2018_0613.UI.Consultas
         public cProyecto()
         {
             InitializeComponent();
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var lista = new List<Proyectos>();
+
+            if (!String.IsNullOrWhiteSpace(CriterioTextBox.Text))
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    case 0:
+                        lista = ProyectosBLL.GetList(r => r.ProyectoId == Utilidades.ToInt(CriterioTextBox.Text));
+                        break;
+                    case 1:
+                        lista = ProyectosBLL.GetList(r => r.Descripcion.Contains(CriterioTextBox.Text.ToUpper()) || r.Descripcion.Contains(CriterioTextBox.Text.ToLower()));
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                lista = ProyectosBLL.GetList(e => true);
+
+            }
+            if (DesdeDataPicker.SelectedDate != null)
+                lista = ProyectosBLL.GetList(c => c.Fecha.Date >= DesdeDataPicker.SelectedDate);
+
+            if (HastaDataPicker.SelectedDate != null)
+                lista = ProyectosBLL.GetList(c => c.Fecha.Date >= HastaDataPicker.SelectedDate);
+
+            DatosDataGrid.ItemsSource = null;
+            DatosDataGrid.ItemsSource = lista;
         }
     }
 }
